48baa38c24abde386ba6ef6dd7c8946ce27a36ac
Mark Diez <markediez@gmail.com>
2017-02-22 15:36:06 -0800

    Get video and audio from getUserMedia

--- public/app.js
+++ public/app.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 	console.log("Hello World");
+
+	var options = {
+		video: true,
+		audio: true
+	}
+
+	navigator.mediaDevices.getUserMedia(options).then(handleSuccess).catch(handleError);
 });
+
+function handleSuccess(stream) {
+	var video = document.querySelector('video');
+	window.stream = stream;
+	video.src = window.URL.createObjectURL(stream);
+}
+
+function handleError(error) {
+	console.log('navigator.getUserMedia error: ', error);
+}
