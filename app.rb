827f0a9181ee5ff79880529393dea962d7c94e5e
Mark Diez <markediez@gmail.com>
2017-02-23 16:07:21 -0800

    Return an image after post

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'  # Managing routes
 require 'data_uri'
+require 'json'
 configure { set :server, :puma }
 
 get '/' do
@@ -10,17 +11,24 @@ get '/stylesheet.css' do
 	scss :stylesheet
 end
 
-get '/app.js' do 
+get '/app.js' do
 	js :app
 end
 
 # http://azemoh.com/2016/05/17/sinatra-managing-file-uploads/
-post '/remove' do 
+post '/remove' do
 	data = params[:img]
 	filename = params[:filename]
-	
-	uri = URI::Data.new(data)
-	File.write("public/uploads/#{filename}", uri.data)	
 
-	"/uploads/#{filename}"
+	uri = URI::Data.new(data)
+	File.write("public/uploads/#{filename}", uri.data)
+
+	# Encode repaired image
+	path = "public/uploads/test.png"
+	returnData = 'data:image/png;base64,'
+	File.open(path, 'rb'){ |file| returnData += Base64.encode64(file.read) }
+
+	# Return most updated image
+	content_type :json
+	{:img => returnData}.to_json
 end
